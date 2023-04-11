fb4ce325c779129b34799885f74f71ba23d5ab55
enpitsulin <enpitsulin@gmail.com>
2023-04-11 10:23:10 +0800

    test: fix test suite for transition-property

--- tests/transform/transition/transition-property.spec.ts
+++ tests/transform/transition/transition-property.spec.ts
@@ -2,41 +2,28 @@ import { transformProperty } from '~/transform'
 
 test('single value', () => {
   expect(transformProperty('transitionProperty', 'none'))
-    .toBe('transition-none')
+    .toBe('transition-property-none')
 
   expect(transformProperty('transitionProperty', 'all'))
-    .toBe('transition-all')
+    .toBe('transition-property-all')
 
   expect(transformProperty('transitionProperty', 'opacity'))
-    .toBe('transition-opacity')
+    .toBe('transition-property-opacity')
 
   expect(transformProperty('transitionProperty', 'box-shadow'))
-    .toBe('transition-shadow')
+    .toBe('transition-property-box-shadow')
 
   expect(transformProperty('transitionProperty', 'transform'))
-    .toBe('transition-transform')
+    .toBe('transition-property-transform')
 })
 
-describe('multiple value', () => {
-  it('transition-property value more than two and both are colors', () => {
-    expect(transformProperty('transitionProperty', 'color, background-color'))
-      .toBe('transition-colors')
+test('multiple value', () => {
+  expect(transformProperty('transitionProperty', 'color, background-color'))
+    .toBe('transition-property-[color,_background-color]')
 
-    expect(transformProperty('transitionProperty', 'outline-color, border-color'))
-      .toBe('transition-colors')
+  expect(transformProperty('transitionProperty', 'outline-color, border-color'))
+    .toBe('transition-property-[outline-color,_border-color]')
 
-    expect(transformProperty('transitionProperty', 'fill, stroke'))
-      .toBe('transition-colors')
-  })
-
-  it('transition-property value more than two and both are default properties', () => {
-    expect(transformProperty('transitionProperty', 'color, box-shadow'))
-      .toBe('transition')
-
-    expect(transformProperty('transitionProperty', 'color, background-color, opacity'))
-      .toBe('transition')
-
-    expect(transformProperty('transitionProperty', 'fill, filter, transform'))
-      .toBe('transition')
-  })
+  expect(transformProperty('transitionProperty', 'fill, stroke'))
+    .toBe('transition-property-[fill,_stroke]')
 })
